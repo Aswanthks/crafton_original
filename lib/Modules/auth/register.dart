aff4bd92bc8568e23a4e156a21ff8a07e8a80550
aswan <aswanthks31@gmail.com>
2024-03-02 16:35:55 +0530

    New3

--- lib/Modules/auth/register.dart
+++ lib/Modules/auth/register.dart
@@ -1,125 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-
 class register extends StatelessWidget {
   const register({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: Text(
-
-          "REGISTER",
-          style: TextStyle(color: Colors.white),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text(
+            "REGISTER",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
 
-      ),
-      body:Container(
-        margin: EdgeInsets.only(left:125),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
 
+          body:  Column(
 
-          children: [
-
-
-
-            Container(
-              height: 200,
-              width: 200,
-
-
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: NetworkImage(
-                      'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png'),
-                  fit: BoxFit.cover,
-                ),
-
-                border: Border.all(
-                  color: Colors.green,
-                  width: 8,
-                ), //Border.all
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: const Offset(
-                      5.0,
-                      5.0,
-                    ), //Offset
-                    blurRadius: 10.0,
-                    spreadRadius: 2.0,
-                  ), //BoxShadow
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: const Offset(0.0, 0.0),
-                    blurRadius: 0.0,
-                    spreadRadius: 0.0,
-                  ), //BoxShadow
-                ],
-              ),
-
-              ),
-
-
-            SizedBox(height: 40,),
-
-
-                Container(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 125),
                 height: 200,
                 width: 200,
-
                 decoration: BoxDecoration(
-                image: const DecorationImage(
-                image: NetworkImage(
-                'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png'),
-        fit: BoxFit.cover,
-      ),
-
-      border: Border.all(
-        color: Colors.green,
-        width: 8,
-      ), //Border.all
-      borderRadius: BorderRadius.circular(15),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black,
-          offset: const Offset(
-            5.0,
-            5.0,
-          ), //Offset
-          blurRadius: 10.0,
-          spreadRadius: 2.0,
-        ), //BoxShadow
-        BoxShadow(
-          color: Colors.white,
-          offset: const Offset(0.0, 0.0),
-          blurRadius: 0.0,
-          spreadRadius: 0.0,
-        ), //BoxShadow
-      ],
-    ),
-
-    ),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png'),
+                    fit: BoxFit.cover,
+                  ),
 
 
+                  border: Border.all(
+                    color: Colors.green,
+                    width: 8,
+                  ), //Border.all
+                  borderRadius: BorderRadius.circular(15),
 
-          ],
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: const Offset(
+                        5.0,
+                        5.0,
+                      ), //Offset
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    ), //BoxShadow
+
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: const Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ), //BoxShadow
+                  ],
+                ),
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.account_box)
+                    
+                  ],                ),
 
 
-        ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 125),
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
 
-      )
 
 
+                  border: Border.all(
+                    color: Colors.green,
+                    width: 8,
+                  ), //Border.all
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      offset: const Offset(
+                        5.0,
+                        5.0,
+                      ), //Offset
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                    ), //BoxShadow
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: const Offset(0.0, 0.0),
+                      blurRadius: 0.0,
+                      spreadRadius: 0.0,
+                    ), //BoxShadow
+                  ],
+                ),
 
 
+              ),
+            ],
 
-
-    );
+        ));
   }
 }
